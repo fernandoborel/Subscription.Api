a83fe9aebe454f12eb4d45829fe793abb88ba75e
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2026-02-27 19:52:17 -0300

    feat(dominio): adiciona as entidades cliente, plano, assinatura bem como baseentity, enums e valueobjects.

---
+++ Subscription.Domain/Enums/Periodicidade.cs
@@ -0,0 +1,9 @@
+﻿namespace Subscription.Domain.Enums
+{
+    public enum Periodicidade
+    {
+        Mensal = 1,
+        Trimestral = 2,
+        Anual = 3,
+    }
+}
---
+++ Subscription.Domain/Enums/StatusAssinatura.cs
@@ -0,0 +1,8 @@
+﻿namespace Subscription.Domain.Enums;
+
+public enum StatusAssinatura
+{
+    Ativa = 1,
+    Cancelada = 2,
+    Suspensa = 3
+}
