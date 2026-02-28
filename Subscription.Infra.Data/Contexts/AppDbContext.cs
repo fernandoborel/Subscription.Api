d06ee53bc221c0f776a2abbe3398d9c4e3372707
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2026-02-27 22:04:04 -0300

    feat(infraestrutura): Entity Framework, mapeamento das entidades, contexto, injeção de dependência e migrations

---
+++ Subscription.Infra.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Subscription.Infra.Data.Configurations;
+
+namespace Subscription.Infra.Data.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options){}
+
+    //Configurar os mapeamentos
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+        modelBuilder.ApplyConfiguration(new PlanoConfiguration());
+        modelBuilder.ApplyConfiguration(new AssinaturaConfiguration());
+    }
+}
